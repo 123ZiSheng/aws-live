c8d10a31acd06218c3cb3242c9dcdf240f56f07f
How Zi Sheng <141216037+123ZiSheng@users.noreply.github.com>
2023-09-19 23:40:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "itp.c4qlbl5ajqxe.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "itp" 
-custombucket = "tangsharren-staff" 
+custombucket = "itp-bucket1" 
 customregion = "us-east-1"
 
